059f848c2f14fcbb5baca80ca16f997743b616d6
Przemysław Kobylański <przemko@mac.com>
2013-04-13 14:32:54 +0200

    Year and name

--- tabsem.pl
+++ tabsem.pl
@@ -1,7 +1,7 @@
 /*
 
     tabsem.pl - method of analytic tableaux for the first order logic
-    Copyright (C) <year>  <name of author>
+    Copyright (C) 2012-2013  Przemyslaw Kobylanski
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
